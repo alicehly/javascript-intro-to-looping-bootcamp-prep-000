edd02a1b41d393861cbbfb2f725948d0414210f1
Alice Ly Ms <alicehly@gmail.com>
2019-05-13 14:38:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -32,7 +32,7 @@ function doWhileLoop(num){
   do {
     console.log('I run once regardless.')
   } while (incrementVariable() <= num);
-
+}
 // function doWhileLoop(num) {
 //   do {
 //     console.log("I run once regardless.");
